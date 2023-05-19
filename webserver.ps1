3cfe05c32c9c564eb72ad6b1eaaa3e870153edac
Razel Rodas <rodas.razel@gmail.com>
2023-05-19 10:49:26 -0700

    Add more complete powershell script.

--- webserver.ps1
+++ webserver.ps1
@@ -8,4 +8,33 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManage
 
 # install git and nano 
 
-choco install git nano
+choco install nano -y
+choco install git.install -y --params "'/GitAndUnixToolsOnPath /WindowsTerminal /NoAutoCrlf'"
+choco install virtualbox -y --params "/NoDesktopShortcut /ExtensionPack"
+choco install multipass -y --params="'/HyperVisor:VirtualBox'"
+
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+refreshenv
+Import-Module "$env:ProgramData\chocolatey\helpers\chocolateyInstaller.psm1"; Update-SessionEnvironment
+
+Write-Host "y" | ssh-keygen -f "./ed25519" -t ed25519 -b 4096 -N `"`"
+
+@"
+#cloud-config
+users:
+  - default
+  - name: $env:username
+    groups: sudo
+    sudo: ALL=(ALL) NOPASSWD:ALL
+    shell: /bin/bash
+    ssh_authorized_keys:
+      - $(cat ./ed25519.pub)
+"@ > cloud-init.yaml
+
+multipass set local.bridged-network=Wi-Fi
+
+Start-Sleep -Seconds 5
+
+multipass launch --name relativepath --cloud-init cloud-init.yaml --bridged
+
+ssh -i ./ed25519 $env:username@$(multipass info relativepath | grep IPv4 | awk '{ print $2 }')
